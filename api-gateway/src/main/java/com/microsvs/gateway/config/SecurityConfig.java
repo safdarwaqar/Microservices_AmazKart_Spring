7df4410bd42bf4b2cc9768fb87e08cd74ccf4b59
SAFDAR WAQAR <safdar.waqar1@gmail.com>
2025-11-04 00:29:48 +0530

    Update SecurityConfig.java
    cors enables

--- api-gateway/src/main/java/com/microsvs/gateway/config/SecurityConfig.java
+++ api-gateway/src/main/java/com/microsvs/gateway/config/SecurityConfig.java
@@ -1,10 +1,15 @@
 package com.microsvs.gateway.config;
 
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsConfigurationSource;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @EnableWebFluxSecurity
@@ -24,4 +29,19 @@ public class SecurityConfig {
 
         return http.build();
     }
-}
+    
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration corsConfig = new CorsConfiguration();
+        corsConfig.setAllowedOrigins(List.of("http://localhost:5173")); // your React app
+        corsConfig.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        corsConfig.setAllowedHeaders(List.of("*"));
+        corsConfig.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfig);
+
+        return source;
+    }
+    
+   }
